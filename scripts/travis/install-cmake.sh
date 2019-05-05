9fb05ae6d92f1e4025799293e0d69b17df6032a5
Victor Timoshin <victor-timoshin@hotmail.com>
2019-05-05 11:16:06 +0300

    Обновлен cmake до 3.14.3

--- scripts/travis/install-cmake.sh
+++ scripts/travis/install-cmake.sh
@@ -1,7 +1,7 @@
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
 	mkdir -p external/cmake
 	pushd external/cmake
-	wget https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.sh
+	wget https://cmake.org/files/v3.14/cmake-3.14.3-Linux-x86_64.sh
 	chmod +x cmake-*-Linux-x86_64.sh
 	./cmake-*-Linux-x86_64.sh --exclude-subdir --skip-license
 	export PATH="${PWD}/bin:$PATH"
